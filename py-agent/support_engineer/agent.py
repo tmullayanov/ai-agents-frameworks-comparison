9ef1f9c0db59ed5fddeb89c7a474b85172d56a2a
Timur Mullayanov <timur.r.mullayanov@gmail.com>
2026-06-10 08:57:21 +0300

    Use universal middleware for both sync and async invocations.

--- py-agent/support_engineer/agent.py
+++ py-agent/support_engineer/agent.py
@@ -3,7 +3,7 @@ from loguru import logger
 
 from langchain.agents import create_agent
 from langchain.tools import tool
-from langchain.agents.middleware import wrap_model_call, ModelRequest, ModelResponse
+from langchain.agents.middleware import wrap_model_call, ModelRequest, ModelResponse, AgentMiddleware
 from typing import Awaitable, Callable
 from langchain.chat_models import init_chat_model
 
@@ -15,28 +15,41 @@ def calculate_factorial(n: int) -> str:
 
 
 
-@wrap_model_call
-async def a_custom_logging_middleware(
-    request: ModelRequest,
-    handler: Callable[[ModelRequest], Awaitable[ModelResponse]],
-):
-    logger.info("[Middleware Input]: Processing query...")
-    outputs = await handler(request)
-    logger.info("[Middleware Output]: Complete.")
-    return outputs
+class CustomLoggingMiddleware(AgentMiddleware):
+    async def awrap_model_call(
+        self,
+        request: ModelRequest,
+        handler: Callable[[ModelRequest], Awaitable[ModelResponse]],
+    ) -> ModelResponse:
+        logger.info("[[Middleware Input]]: Processing query...")
+        outputs = await handler(request)
+        logger.info("[[Middleware Output]]: Complete.")
+        return outputs    
+    
+
+    def wrap_model_call(
+            self,
+            request: ModelRequest,
+            handler: Callable[[ModelRequest], ModelResponse]
+    ) -> ModelResponse:
+        logger.info("[[Middleware Input]]: Processing query...")
+        outputs = handler(request)
+        logger.info("[[Middleware Output]]: Complete.")
+        return outputs
+
 
 logger.info("creating the model via OpenAI interface")
 
 model = init_chat_model(
     base_url="http://127.0.0.1:1234/v1",
     api_key="not-needed",
-    model="qwen/qwen3.5-9b",
+    model="qwen/qwen3-4b",
     model_provider="openai"
 )
 
 agent = create_agent(
     model,
     tools=[calculate_factorial],
-    middleware=[a_custom_logging_middleware],
+    middleware=[CustomLoggingMiddleware()],
     system_prompt="You are a helpful assistant"
 )
