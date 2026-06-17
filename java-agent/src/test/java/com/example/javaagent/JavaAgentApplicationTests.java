5ac57b47bb5077df1db81e31ab667c5ce26e5f5f
Timur Mullayanov <timur.r.mullayanov@gmail.com>
2026-06-17 08:57:49 +0300

    [java-agent] Add basic Spring Boot skeleton

---
+++ java-agent/src/test/java/com/example/javaagent/JavaAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
