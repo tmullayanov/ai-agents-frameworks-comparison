3987fed26b3512fb9b2e63f7e456501478c3cc8b
Timur Mullayanov <tmullayanov@users.noreply.github.com>
2026-07-03 11:43:30 +0300

    [py-agent] Add more civilized CLI-friendly way to run agent

--- py-agent/main.py
+++ py-agent/main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from py-agent!")
+import uvicorn
 
+from support_engineer.http_api import app
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
